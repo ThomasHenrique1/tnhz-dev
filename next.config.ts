d8c653e008a5b706364206406ae78082b33bc518
Tom <thomasnhenrique@gmail.com>
2026-04-15 20:48:04 -0300

    Atualizando config do nextJS para rodar por IP

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-require-imports */
 import type { NextConfig } from "next";
 
-const withMDX = require('@next/mdx')({
+const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
 
 const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  allowedDevOrigins: ["192.168.15.40"],
+  
+
 };
 
 export default withMDX(nextConfig);
